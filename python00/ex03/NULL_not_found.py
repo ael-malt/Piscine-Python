9838c850fa5cb7aa98093ec5ec9098a113da37a1
ael-malt <thenono1111@gmail.com>
2025-10-07 19:30:21 +0200

    Finished P00/ex03

--- python00/ex03/NULL_not_found.py
+++ python00/ex03/NULL_not_found.py
@@ -1,3 +1,18 @@
 def NULL_not_found(object: any) -> int:
-	print(object)
-	return
+	object_type = type(object)
+	if object is None:
+		print(f"Nothing : None {object_type}")
+	if object_type is float:
+		print(f"Cheese : nan {object_type}")
+	elif object_type is int and object == 0:
+		print(f"Zero : 0 {object_type}")
+	elif object_type is str and object == '':
+		print(f"Empty : {object_type}")
+	elif not object and object_type is bool:
+		print(f"Fake : False {object_type}")
+	else:
+		print("Type not found")
+		return 1
+	# print(object)
+	# always return 0
+	return 0
